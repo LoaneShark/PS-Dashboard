038fed1db116ed0767909306528bb4235679adbd
Santiago Loane <santiago.loane@rochester.edu>
2020-01-28 14:04:26 -0500

    Querying
    - Successful DB Query being executed
    - .txt file containing sample queries and links to suggested/previously used graphs

--- app/components/DBQuery.js
+++ app/components/DBQuery.js
@@ -41,8 +41,10 @@ export default class DBQuery extends Component<Props> {
       const connection: ConnectionPool = new client.ConnectionPool(this.config)
         .connect()
         .then(pool => {
-          return pool.query('select * from dbo.EMP').then(res => {
+          return pool.query(testQuery1()).then(res => {
             console.log(res);
+            console.log(res.recordset);
+            console.log(res.recordset.toTable());
             return res;
           });
         })
@@ -65,3 +67,49 @@ export default class DBQuery extends Component<Props> {
     }
   }
 }
+
+const testQuery1 = function testQuery1() {
+  return `SELECT        dbo.ACCESSPANE.NAME AS Panel, dbo.ALARMPANEL.NAME AS [Alarm Panel/Reader], dbo.ALARMINPUT.NAME AS Input, dbo.EVENT.EVDESCR AS Event, COUNT(dbo.EVENTS.SERIALNUM) AS Count,
+                         dbo.ALARM.ALPRIORITY AS Priority, DATEPART(DD,
+                         dbo.EVENTS.EVENT_TIME_UTC) AS Day, DATEPART(MM, dbo.EVENTS.EVENT_TIME_UTC) AS Month
+  FROM            dbo.EVENT RIGHT OUTER JOIN
+                           dbo.EVENTS LEFT OUTER JOIN
+                           dbo.ALARM INNER JOIN
+                           dbo.EVTALMLINK ON dbo.ALARM.ALID = dbo.EVTALMLINK.ALID ON dbo.EVENTS.DEVID = dbo.EVTALMLINK.DEVICEID AND dbo.EVENTS.INPUTDEVID = dbo.EVTALMLINK.INPUTDEVID AND
+                           dbo.EVENTS.MACHINE = dbo.EVTALMLINK.PANELID AND dbo.EVENTS.EVENTID = dbo.EVTALMLINK.EVID AND dbo.EVENTS.EVENTTYPE = dbo.EVTALMLINK.EVTYPEID ON
+                           dbo.EVENT.EVID = dbo.EVENTS.EVENTID AND dbo.EVENT.EVTYPEID = dbo.EVENTS.EVENTTYPE LEFT OUTER JOIN
+                           dbo.ALARMINPUT ON dbo.EVENTS.MACHINE = dbo.ALARMINPUT.PANELID AND dbo.EVENTS.DEVID = dbo.ALARMINPUT.ALARMPID AND dbo.EVENTS.INPUTDEVID = dbo.ALARMINPUT.INPUTID LEFT OUTER JOIN
+                           dbo.ACCESSPANE ON dbo.EVENTS.MACHINE = dbo.ACCESSPANE.PANELID LEFT OUTER JOIN
+                           dbo.ALARMPANEL ON dbo.EVENTS.MACHINE = dbo.ALARMPANEL.PANELID AND dbo.EVENTS.DEVID = dbo.ALARMPANEL.ALARMPID
+  WHERE        (dbo.ALARMPANEL.NAME IS NOT NULL) AND (dbo.EVENTS.EVENTTYPE = 4) AND (dbo.EVENTS.EVENTID = 2)and events.event_time_utc > getdate()-360
+  GROUP BY dbo.ALARMINPUT.NAME, dbo.ALARMPANEL.NAME, dbo.ACCESSPANE.NAME, dbo.ALARM.ALPRIORITY, dbo.EVENT.EVDESCR,  dbo.EVENTS.EVENT_TIME_UTC
+
+  Union all
+
+  SELECT        dbo.ACCESSPANE.NAME AS Panel, dbo.READER.READERDESC AS [Alarm Panel], dbo.READER.AUX1NAME AS Input, dbo.EVENT.EVDESCR AS Event, COUNT(dbo.EVENTS.SERIALNUM) AS Count,
+                           dbo.ALARM.ALPRIORITY, DATEPART(DD, dbo.EVENTS.EVENT_TIME_UTC) AS Day, DATEPART(MM, dbo.EVENTS.EVENT_TIME_UTC) AS Month
+  FROM            dbo.EVENTS LEFT OUTER JOIN
+                           dbo.ALARM INNER JOIN
+                           dbo.EVTALMLINK ON dbo.ALARM.ALID = dbo.EVTALMLINK.ALID ON dbo.EVENTS.EVENTTYPE = dbo.EVTALMLINK.EVTYPEID AND dbo.EVENTS.EVENTID = dbo.EVTALMLINK.EVID AND
+                           dbo.EVENTS.MACHINE = dbo.EVTALMLINK.PANELID AND dbo.EVENTS.DEVID = dbo.EVTALMLINK.DEVICEID AND dbo.EVENTS.INPUTDEVID = dbo.EVTALMLINK.INPUTDEVID LEFT OUTER JOIN
+                           dbo.READER ON dbo.EVENTS.MACHINE = dbo.READER.PANELID AND dbo.EVENTS.DEVID = dbo.READER.READERID LEFT OUTER JOIN
+                           dbo.ACCESSPANE ON dbo.EVENTS.MACHINE = dbo.ACCESSPANE.PANELID LEFT OUTER JOIN
+                           dbo.EVENT ON dbo.EVENTS.EVENTID = dbo.EVENT.EVID AND dbo.EVENTS.EVENTTYPE = dbo.EVENT.EVTYPEID
+  WHERE        (dbo.READER.AUX1NAME IS NOT NULL) AND (dbo.EVENTS.EVENTTYPE = 4) AND (dbo.EVENTS.EVENTID = 2) AND (dbo.EVENTS.INPUTDEVID = 1)and events.event_time_utc > getdate()-360
+  GROUP BY dbo.EVENT.EVDESCR, dbo.READER.AUX1NAME, dbo.READER.READERDESC, dbo.ACCESSPANE.NAME, dbo.ALARM.ALPRIORITY, DATEPART(MM, dbo.EVENTS.EVENT_TIME_UTC), DATEPART(DD,
+                           dbo.EVENTS.EVENT_TIME_UTC)
+
+  Union all
+  SELECT        dbo.ACCESSPANE.NAME AS Panel, dbo.READER.READERDESC AS [Alarm Panel], dbo.READER.AUX2NAME AS Input, dbo.EVENT.EVDESCR AS Event, COUNT(dbo.EVENTS.SERIALNUM) AS Count,
+                           dbo.ALARM.ALPRIORITY, DATEPART(DD, dbo.EVENTS.EVENT_TIME_UTC) AS Day, DATEPART(MM, dbo.EVENTS.EVENT_TIME_UTC) AS Month
+  FROM            dbo.EVENTS LEFT OUTER JOIN
+                           dbo.ALARM INNER JOIN
+                           dbo.EVTALMLINK ON dbo.ALARM.ALID = dbo.EVTALMLINK.ALID ON dbo.EVENTS.EVENTTYPE = dbo.EVTALMLINK.EVTYPEID AND dbo.EVENTS.EVENTID = dbo.EVTALMLINK.EVID AND
+                           dbo.EVENTS.MACHINE = dbo.EVTALMLINK.PANELID AND dbo.EVENTS.DEVID = dbo.EVTALMLINK.DEVICEID AND dbo.EVENTS.INPUTDEVID = dbo.EVTALMLINK.INPUTDEVID LEFT OUTER JOIN
+                           dbo.READER ON dbo.EVENTS.MACHINE = dbo.READER.PANELID AND dbo.EVENTS.DEVID = dbo.READER.READERID LEFT OUTER JOIN
+                           dbo.ACCESSPANE ON dbo.EVENTS.MACHINE = dbo.ACCESSPANE.PANELID LEFT OUTER JOIN
+                           dbo.EVENT ON dbo.EVENTS.EVENTID = dbo.EVENT.EVID AND dbo.EVENTS.EVENTTYPE = dbo.EVENT.EVTYPEID
+  WHERE        (dbo.READER.AUX2NAME IS NOT NULL) AND (dbo.EVENTS.EVENTTYPE = 4) AND (dbo.EVENTS.EVENTID = 2) AND (dbo.EVENTS.INPUTDEVID = 2)and events.event_time_utc > getdate()-360
+  GROUP BY dbo.EVENT.EVDESCR, dbo.READER.AUX2NAME, dbo.READER.READERDESC, dbo.ACCESSPANE.NAME, dbo.ALARM.ALPRIORITY, DATEPART(MM, dbo.EVENTS.EVENT_TIME_UTC), DATEPART(DD,
+                           dbo.EVENTS.EVENT_TIME_UTC)`;
+};
